a6b686017f5d0a85d12486ceeaacded2d14f8cfe
CodeAndPranks <140973619+CodeAndPranks@users.noreply.github.com>
2024-11-04 15:57:19 +0100

    Update app.js
    Removed  "\n" (new line) who didn't workethe  for "output text".
    IAnd I move to "output" div above the image make sure user se it .

--- app.js
+++ app.js
@@ -1,25 +1,21 @@
-
-
- 
+function myFunc ()  {
 let timePerLetter = 30,
-   // newLineCharacter = '|',
    text = document.createTextNode('');
  
 document.getElementById("test").appendChild(text);
 document.getElementById("test").style.color="yellow"
 document.body.style.backgroundColor="black"
 
- 
 function printOut(str) {
   var i = 0;
   (function main() {
     var char = str[i++];
-    text.nodeValue += char //== newLineCharacter ? '\n' : char;
+    text.nodeValue += char
     if(i < str.length)
       setTimeout(main, timePerLetter);
   })();
 }
-
-printOut("...Your hardisk is empty please install a OS before you can use this unit\n All Coyrihgts reserved by \nFed.11/99-ADv/SACR.USA Dist.of.Alabm 047\n \nHP Internactional,Sacramento,California USA. ")
-  
+printOut("...Your hardisk is empty please install a OS before you can use this uni. Coyright reserved by: Fed.11/99-ADv/SACR.USA Dist.of.Alabm.047.HP Internactional,Sacramento,California USA. ")
+}
+myFunc()
 
