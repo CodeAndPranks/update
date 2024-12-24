d6864383288c6d2325717b5dc40c79f7701caa92
CodeAndPranks <140973619+CodeAndPranks@users.noreply.github.com>
2024-12-25 00:38:28 +0100

    Update app.js
    test

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
-function myFunc ()  {
-let timePerLetter = 30,
-   text = document.createTextNode('');
- 
-document.getElementById("test").appendChild(text);
-document.getElementById("test").style.color="yellow"
-document.body.style.backgroundColor="black"
 
-function printOut(str) {
-  var i = 0;
-  (function main() {
-    var char = str[i++];
-    text.nodeValue += char
-    if(i < str.length) 
-  })();
+function myFunc() {
+  let timePerLetter = 30,
+    text = document.createTextNode('');
+
+  document.getElementById("test").appendChild(text);
+  document.getElementById("test").style.color = "yellow";
+  document.body.style.backgroundColor = "black";
+
+  function printOut(str) {
+    var i = 0;
+
+    function main() {
+      if (i < str.length) {
+        var char = str[i++];
+        text.nodeValue += char;
+      } else {
+        // Add a 2-second delay after the text is finished
+        setTimeout(function() {
+          text.nodeValue = '';
+           
+          document.body.style.backgroundColor = "grey";
+        }, 2000);
+      }
+    }
+
+    if (i < str.length) {
+      setTimeout(main, timePerLetter);
+    }
+  }
+
+  printOut("...Your hardisk is empty please install a OS before you can use this unit\n All Coyrihgts reserved by \nFed.11/99-ADv/SACR.USA Dist.of.Alabm 047\n \nHP Internactional,Sacramento,California USA. ");
 }
-printOut("...Your hardisk is empty please install a OS before you can use this unit\n All Coyrihgts reserved by \nFed.11/99-ADv/SACR.USA Dist.of.Alabm 047\n \nHP Internactional,Sacramento,California USA. ")
-}
-
-
