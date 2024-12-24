c933aafa80f11af20c5aebc85d6129ec922c1b52
CodeAndPranks <140973619+CodeAndPranks@users.noreply.github.com>
2024-12-25 00:30:29 +0100

    Update app.js
    bug

--- app.js
+++ app.js
@@ -11,20 +11,10 @@ function printOut(str) {
   (function main() {
     var char = str[i++];
     text.nodeValue += char
-    if(i < str.length)
-      setTimeout(main, timePerLetter);
-   else 
-   setTimeout(function() {
-     text.nodeValue = ""
-     demo.innerHTML = ""
-     h2.innerHTML = " "
-     document.body.style.backgroundColor="grey"
-  }, 2000);  
+    if(i < str.length) 
   })();
 }
 printOut("...Your hardisk is empty please install a OS before you can use this unit\n All Coyrihgts reserved by \nFed.11/99-ADv/SACR.USA Dist.of.Alabm 047\n \nHP Internactional,Sacramento,California USA. ")
 }
-setTimeout(function() {
-    myFunc();
- }, 2000);
+
 
