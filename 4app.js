6609448ecb745a9f0959b8e3ca052393db1e365e
CodeAndPranks <140973619+CodeAndPranks@users.noreply.github.com>
2024-12-25 01:32:27 +0100

    Create 4app.js
    4'th js file

---
+++ 4app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+function myFunc() {
+  let timePerLetter = 30;
+
+  function printOut(str) {
+    var i = 0;
+
+    function main() {
+      if (i < str.length) {
+        var char = str[i++];
+        text.nodeValue += char;
+      } else {
+        // Add a 2-second delay after the text is finished
+        setTimeout(function () {
+          text.nodeValue = '';
+          document.body.style.backgroundColor = "grey";
+        }, 2000);
+      }
+    }
+
+    if (i < str.length) {
+      setTimeout(main, timePerLetter);
+    }
+  }
+
+  var text = document.createTextNode('');
+  document.getElementById("test").appendChild(text);
+  document.getElementById("test").style.color = "yellow";
+  document.body.style.backgroundColor = "black";
+
+  printOut("...Your hardisk is empty please install a OS before you can use this unit\n All Coyrihgts reserved by \nFed.11/99-ADv/SACR.USA Dist.of.Alabm 047\n \nHP Internactional,Sacramento,California USA. ");
+}
+
+myFunc();
